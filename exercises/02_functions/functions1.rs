cf8da5f06c0d0e7f0234b90e142d6735d84c80fe
Armughan Mehmood <armughanofficial20@gmail.com>
2026-04-21 01:54:28 +0500

    Completed Variable, Move Semantics, Structs, and Enums

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,9 @@
+
+fn call_me() 
+{
+    println!("You called me!");
+}
+
+fn main() {
+    call_me(); // Don't change this line
+}
---
+++ exercises/03_if/if1.rs
@@ -0,0 +1,39 @@
+fn bigger(a: i32, b: i32) -> i32 {
+   if a > b
+   {
+    a
+   }
+   else 
+   {
+    b
+   }
+   
+}
+
+fn main() {
+    
+    let a = 5;
+    let b = 8;
+    let result = bigger (a,b);
+    println!("The bigger number between {} and {} is {}", a, b, result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ten_is_bigger_than_eight() {
+        assert_eq!(10, bigger(10, 8));
+    }
+
+    #[test]
+    fn fortytwo_is_bigger_than_thirtytwo() {
+        assert_eq!(42, bigger(32, 42));
+    }
+
+    #[test]
+    fn equal_numbers() {
+        assert_eq!(42, bigger(42, 42));
+    }
+}
